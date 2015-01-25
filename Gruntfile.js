ac5b7102b66c7cf70ffc8bfc161b77390289040a
Dany Shaanan <danyshaanan@gmail.com>
2015-01-25 20:50:15 +0200

    added eslint

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      target: ['lib/**/*.js']
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'npm-shrinkwrap.json'],
+        commitFiles: ['package.json', 'npm-shrinkwrap.json'],
+        tagName: '%VERSION%',
+        push: false
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-eslint')
+  grunt.loadNpmTasks('grunt-bump')
+
+  grunt.registerTask('default', ['eslint'])
+};
